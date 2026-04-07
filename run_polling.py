f164fffe1c76e54043e59703768df94bf2e04616
Lu <131333305+KrotLu@users.noreply.github.com>
2026-04-07 12:09:14 +0500

    Create run_polling.py
    Add polling launcher

---
+++ run_polling.py
@@ -0,0 +1,20 @@
+import asyncio
+import os
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher
+from handlers import user
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN not set")
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+    dp.include_router(user)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
